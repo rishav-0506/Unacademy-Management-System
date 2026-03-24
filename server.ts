863150893e69fedbb152effea394505e9d472cbb
sahdevkumar <internet.00090@gmail.com>
2026-03-25 02:03:21 +0530

    feat: Update wildcard route handler
    Changes the wildcard route handler from `*` to `*all` to prevent accidental matching of internal server routes and ensure correct serving of the SPA's `index.html` for all client-side routes.

--- server.ts
+++ server.ts
@@ -95,7 +95,7 @@ async function setupVite(app: any) {
   } else {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
+    app.get('*all', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
